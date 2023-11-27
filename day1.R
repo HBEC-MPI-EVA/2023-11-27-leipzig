8a3ccd4b465f8508e81c9bb6f7a06504001bc974
Bret Beheim <beheim@gmail.com>
2023-11-27 18:27:36 +0100

    updated day1 code with annotations

--- day1.R
+++ day1.R
@@ -1,21 +1,29 @@
+#running lines of code in script and console
 6*7
+
+#functions have arguments, they can be obligatory or optional. Optional arguments have default values
 print("Ciao Leipzig")
 print(x = "Ciao Leipzig")
 print(x = "Ciao Leipzig", quote = FALSE)
 print(x = "Hello Kitty")
 
+#assignment
 six_times_seven <- 6*7
 
 print(x = six_times_seven)
 
+#installing and loading packages
 install.packages("ggplot2")
 library(ggplot2)
 
+#calling on functions from a specific packages
 ggplot()
 
 ggplot2::ggplot()
 
 #data types and structures
+
+#numeric and character
 greetings_city_vec <- c("Ciao leipzig", "Goodbye Berlin", "Hi Dresden")
 
 greetings_city_vec[3]
@@ -30,14 +38,17 @@ chr_vec <- c("1", "2", "3", "HEDVIG")
 
 as.numeric(chr_vec)
 
+#What happens if the vector has nothing in it, but a comma?
 output <- empty_vec <- c(,)
 
 as.numeric(empty_vec)
 
 as.character(num_vec)
 
+#logical vectors
 log_vec <- as.numeric(c(TRUE, FALSE))
 
+#TRUE = 1 and FALSE = 0
 sum(log_vec)
 
 num_log_vec <- as.numeric(log_vec)
@@ -47,6 +58,7 @@ as.character(log_vec)
 name_vec <- c("Angela", "Hedvig", "Ezequiel")
 sort(name_vec)
 
+#factors
 name_factor_vec <- factor(x = c("Angela", "Hedvig", "Ezequiel", "Hedvig", "Bret", "Hedwig"), 
                           levels = c("Ezequiel", "Hedvig", "Angela"))
 name_factor_vec
@@ -59,20 +71,25 @@ names_as_numbers <- as.numeric(name_vec)
 
 as.numeric(name_factor_vec)
 
+#lists are 1-dimensional objects where the items can be of different types.
 list <- list("hedvig", TRUE, 1)
 
 str(list)
 
+#data-frames
 gapminder <- read.csv("https://raw.githubusercontent.com/swcarpentry/r-novice-gapminder/main/episodes/data/gapminder_data.csv")
 
 str(gapminder)
 
+#selecting a subset of a data-frame. 
+#rows, columns
 gapminder[3:6,4:6]
 
 sum(gapminder$pop)
 
 not_afgan_gapminder <- subset(x = gapminder, country != "Afghanistan")
 
+#different kinds of non-data
 empty <- c()
 empty_2 <- c("", "", NA)
 str(empty_2)
@@ -93,21 +110,24 @@ sum(year_1992_dataframe$pop)
 
 year_1992_dataframe$pop
 
-format(max(year_1992_dataframe$pop), big.mark = ",")
-
+#changing large numbers to strings that are easier for human eyes to parse
 format(year_1992_dataframe$pop, big.mark = ",")
 
+format(max(year_1992_dataframe$pop), big.mark = ",")
+
+#what happens if we replace the column in the dataframe with a changed version of the column where we've formatted it to be character strings?
 year_1992_dataframe$pop <- format(year_1992_dataframe$pop, big.mark = ",")
 
 sum(year_1992_dataframe$pop)
 
+#picking out the particular row that meets a condition
 subset(year_1992_dataframe, pop == max(year_1992_dataframe$pop))
 
 subset(year_1992_dataframe, pop == 3333333333333333333333)
 
 subset(year_1992_dataframe, country == "afganistan")
 
-year_1992_dataframe[year_1992_dataframe$pop == max(year_1992_dataframe$pop),4]
+year_1992_dataframe[year_1992_dataframe$pop == max(year_1992_dataframe$pop),]
 
 year_1992_dataframe[, "country"]
 year_1992_dataframe$country
@@ -116,6 +136,7 @@ max(year_1992_dataframe[,3])
 
 str(year_1992_dataframe$pop)
 
+#digging deeper into how max() and sort() treats non-numeric data-types
 chara_vector <- c("Hedvig", "Ezequiel")
 
 max(chara_vector)
@@ -128,26 +149,28 @@ str(year_1992_dataframe)
 table(year_1992_dataframe$pop)
 unique(year_1992_dataframe$pop)
 
+#functions for testing if something is a data-type
 is.numeric(letter_vector)
 is.character(letter_vector)
 is.factor(letter_vector)
 
 letter_vector <- c("A","b", "AA", "B","c", "3", NA, NULL, "   b", "   ", NaN)
 
-is.na(letter_vector)
+is.na(`letter_vector`)
 
-letter vector <- c("A","b", "AA", "B","c", "3", NA, NULL, "   b", "   ", NaN, NULL, NULL, NULL)
-print(letter vector)
+#it is possible (but NOT adivsable) to have spaces in variable names. if this happens, use backticks (and preferably rename as soon as possible).
+`letter vector` <- c("A","b", "AA", "B","c", "3", NA, NULL, "   b", "   ", NaN, NULL, NULL, NULL)
+print(`letter vector`)
 
 is.na(c("letter_vector"))
 
 length(c("letter_vector"))
 length(letter_vector)
-length(letter vector)
-length(letter vector)
+length(`letter vector`)
+length(`letter vector`)
 
 length(c(NA, "hello"))
 
-is.nan(letter vector)
+is.nan(`letter vector`)
 
 str(c(1, "hedvig",TRUE))
