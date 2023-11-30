3d31f4a843657dfadfc5a7f800bad5ccefde5be9
Bret Beheim <bret_beheim@eva.mpg.de>
2023-11-30 13:03:45 +0100

    updated day2 materials with alexs ggplot2stuff

--- day2.R
+++ day2.R
@@ -114,10 +114,111 @@ gapminder %>%
   ) %>% 
   as.data.frame()
 
-gapminder <- read.csv("data/gapminder_data.csv")
 
+
+# Download the gapminder dataset from the internet
+download.file("https://raw.githubusercontent.com/swcarpentry/r-novice-gapminder/main/episodes/data/gapminder_data.csv",
+              destfile = "data/gapminder_data.csv")
+gapminder <- read.csv("data/gapminder.csv")
+
+# Install packages if not yet
+install.packages("ggplot2")
+install.packages("dplyr")
+# Load packages
 library(ggplot2)
+library(dplyr)
 
+# Initial plot: plot life expectancy over GDP per capita
+## Add data
+ggplot(data = gapminder)  # empty pane
+## Add mapping aesthestics additionally
 ggplot(data = gapminder,
-       mapping = aes(x = gdpPercap, y = lifeExp))
+       mapping = aes(x = year, y = lifeExp)) # empty pane with axes set to minimum and maximum values
+## Add plotting layer
+ggplot(data = gapminder,
+       mapping = aes(x = year, y = lifeExp)) +
+geom_point() # scatter plot
 
+# Hint: data and mapping are not necessary
+ggplot(gapminder,
+       aes(x = year, y = lifeExp)) +
+geom_point()
+
+# New plot: plot life expectancy over years
+ggplot(gapminder, aes(x = year, y = lifeExp)) +
+geom_point()
+
+# Add colour by changing the colour of points by continent
+ggplot(gapminder, aes(x = year, y = lifeExp, colour = continent)) +
+geom_point()
+
+# Instead of a scatter plot, highlight the change over time with lines
+ggplot(data = gapminder,
+       mapping = aes(x = year, y = lifeExp,
+                     group = country, colour = continent)) +
+  geom_line()
+
+# We can plot both lines and points
+ggplot(data = gapminder,
+       mapping = aes(x = year, y = lifeExp,
+                     group = country, colour = continent)) +
+  geom_line() +
+  geom_point() 
+
+# To avoid having both the colours and lines, we can move the colour aesthetics 
+# to geom_line
+ggplot(data = gapminder,
+       mapping = aes(x = year, y = lifeExp)) +
+  geom_line(aes(group = country, colour = continent)) +
+  geom_point()
+
+# In case, you want different colour schemes for lines and points, you can use "fill" for
+# points instead
+# This requires us to set the shape to specific shapes that can be filled; you can see an
+# overview of all shapes by executing "?pch" in the R console
+ggplot(data = gapminder,
+       mapping = aes(x = year, y = lifeExp)) +
+  geom_line(aes(group = country, colour = continent)) +
+  geom_point(aes(fill = continent, shape = continent), colour = "grey30") +
+  scale_shape_manual(values = c(21, 22, 23, 24, 25))
+  scale_colour_manual()
+
+# Colour names can be specified either by R's known colour names or by their HEX code
+# You can find the list of all known R colours here: https://r-charts.com/colors/
+  
+# Perform simple statistical analysis: linear regression between gdpPercap and lifeExp
+# prior to the statistical analysis, we will log10-transform the gdpPercap variable to
+# obtain a quasi-linear relationship between the variables
+  ggplot(gapminder, aes(x = gdpPercap, y = lifeExp)) +
+    geom_point(alpha = 0.3) +
+    geom_smooth(method = "lm", linewidth = 2.5, colour = "#FF4040") +
+    scale_x_log10()
+  
+# You can get an overview of which aesthestics you can change for a given geom_ layer
+# in ggplot, run "?geom_point" or the name of geom layer in your R console and scroll
+# towards the section Aesthestics in the help menu
+  
+# Let's plot the change of the life expectancy over time per country, displaying each
+# country in its own little plot
+lifexp_plt <- gapminder %>%
+  filter(continent == "Americas") %>%
+  ggplot(aes(x = year, y = lifeExp)) +
+  geom_line(aes(group = country)) +
+  labs( # Allows you to change the name of the axes title, the legend title or set the plot title
+    y = "life expectancy [y]",
+    title = "Development of the life expectancy in the Americas (1952-2007)"
+  ) +
+  facet_wrap(~ country) +
+  # theme allows you to manipulate the parts of the plot that are independent of data
+  theme(axis.text.x = element_text(angle = 90, hjust = 1, vjust = 0.5),
+        strip.background = element_blank())
+lifexp_plt
+  
+# Save plot to PDF
+ggsave("~/Desktop/lifexp_development_americas.pdf",
+       plot = lifexp_plt,
+       width = 12,
+       height = 8,
+       units = "cm",
+       useDingbats = FALSE)
+# Reason why to always use "useDingbats = FALSE": https://stackoverflow.com/questions/9992275/ggplot2-pdf-import-in-adobe-illustrator-missing-font-adobepistd
